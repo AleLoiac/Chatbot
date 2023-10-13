03a9c6d4affc1616f2e83139b06167fc2baed2f9
AleLoiac <alessandroloiac@gmail.com>
2023-10-13 14:04:33 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module Chatbot
+
+go 1.21.1
+
+require (
+	github.com/go-resty/resty/v2 v2.9.1
+	github.com/wit-ai/wit-go/v2 v2.0.2
+)
+
+require golang.org/x/net v0.17.0 // indirect
---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"github.com/go-resty/resty/v2"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Hello! I'm your chatbot. How can I assist you today?")
+
+	// Create an HTTP client for making API requests
+	client := resty.New()
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("You: ")
+		scanner.Scan()
+		input := scanner.Text()
+
+		// Convert the user input to lowercase for case-insensitive matching
+		input = strings.ToLower(input)
+
+		// Use Wit.ai for natural language understanding
+		intent, err := getWitIntent(client, input)
+
+		// Based on the intent read by Wit get an answer hard coded in the function
+		if err != nil {
+			fmt.Println("Chatbot: I'm not sure how to respond to that.")
+		} else {
+			response := getResponse(intent)
+			fmt.Println("Chatbot:", response)
+		}
+	}
+}
+
+// getWitIntent sends the user input to Wit.ai and extracts the detected intent
+func getWitIntent(client *resty.Client, input string) (string, error) {
+
+	// Set your Wit.ai API token
+	const witToken = "VCI3NXCT645IGUORIU3MHET2YLDNXQON"
+	const witURL = "https://api.wit.ai/message"
+
+	// Make a GET request to Wit.ai with the user input
+	resp, err := client.R().
+		SetHeader("Authorization", "Bearer "+witToken).
+		SetQueryParams(map[string]string{
+			"q": input,
+		}).
+		Get(witURL)
+
+	if err != nil {
+		return "", err
+	}
+
+	// Parse the JSON response from Wit.ai
+	var witResponse map[string]interface{}
+	err = json.Unmarshal(resp.Body(), &witResponse)
+	if err != nil {
+		return "", err
+	}
+
+	// Extract the detected intent
+	intent, ok := witResponse["intents"].([]interface{})
+	if !ok || len(intent) == 0 {
+		return "", nil
+	}
+
+	firstIntent, ok := intent[0].(map[string]interface{})
+	if !ok {
+		return "", nil
+	}
+
+	return firstIntent["name"].(string), nil
+}
+
+// getResponse generates a response based on the detected intent
+func getResponse(intent string) string {
+
+	// Define a response library with predefined responses for different intents
+	responseLibrary := map[string]string{
+		"greet":     "Hello! How can I help you?",
+		"introduce": "I'm a chatbot written in Go.",
+		"goodbye":   "Goodbye! Have a great day.",
+		"default":   "I'm not sure how to respond to that.",
+	}
+
+	// Retrieve the appropriate response from the library or use a default response
+	response, ok := responseLibrary[intent]
+	if !ok {
+		response = responseLibrary["default"]
+	}
+	return response
+}
